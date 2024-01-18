19101ec726b200e98caef9a000de51b727194127
Mitchell Wang <mitchell_wang@outlook.com>
2024-01-18 19:24:07 +1100

    started coding the main classes

---
+++ src/main.js
@@ -0,0 +1,15 @@
+function gameBoard () {
+    let gameboard = [[" "," "," "],[" "," "," "][" "," "," "]];
+    const getGameBoard = function () {
+        return gameboard
+    }
+    const updateGameBoard = function (player, position) {
+        
+    }
+    return { getGameBoard, updateGameBoard };
+}
+
+function player(id, name) {
+    const playerId = id; 
+    const playerName = name
+}
